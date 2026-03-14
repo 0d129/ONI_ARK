fd3cc29174798e20b7bf9ad321798dab9b5bc99b
0d129 <swe5d@outlook.com>
2026-03-14 15:08:14 +0800

    使用rider格式化，以后估计用rider开发了

--- ONI_ARK/Patches.cs
+++ ONI_ARK/Patches.cs
@@ -7,13 +7,10 @@ namespace ONI_ARK
 {
     public class Patches
     {
-
         [HarmonyPatch(typeof(Db))]
         [HarmonyPatch("Initialize")]
         public class Db_Initialize_Patch
         {
-        
-
             public static void Prefix()
             {
                 Debug.Log("Cuora execute before Db.Initialize!");
@@ -29,6 +26,7 @@ namespace ONI_ARK
                     Debug.LogWarning($"在 kanim 中没有找到名为 {name} 的 Symbol！请检查你打包的贴图和 scml 文件名。");
                     return;
                 }
+
                 Accessory myCustomShape = new Accessory(
                     id: name,
                     parent: Db.Get().Accessories,
@@ -44,7 +42,6 @@ namespace ONI_ARK
 
             public static void Postfix()
             {
-                
                 Debug.Log("Cuora execute after Db.Initialize!");
                 KAnimFile customKanim = Assets.GetAnim("char_150_snakek_kanim");
                 if (customKanim == null)
@@ -52,6 +49,7 @@ namespace ONI_ARK
                     Debug.LogWarning("未找到自定义 Kanim，皮肤可能加载失败。");
                     return;
                 }
+
                 // 【关键防坑一步】强制初始化动画数据，确保我们能取到 Symbol
                 //customKanim.Initialize();
                 LoadSymbol(customKanim, "headshape_1150", Db.Get().AccessorySlots.HeadShape);
@@ -60,6 +58,7 @@ namespace ONI_ARK
                 LoadSymbol(customKanim, "belt_1150", Db.Get().AccessorySlots.Belt);
                 LoadSymbol(customKanim, "neck_1150", Db.Get().AccessorySlots.Neck);
                 LoadSymbol(customKanim, "hair_1150", Db.Get().AccessorySlots.Hair);
+                LoadSymbol(customKanim, "hat_hair_1150", Db.Get().AccessorySlots.HatHair);
                 LoadSymbol(customKanim, "cuff_1150", Db.Get().AccessorySlots.Cuff);
                 LoadSymbol(customKanim, "foot_1150", Db.Get().AccessorySlots.Foot);
                 LoadSymbol(customKanim, "hand_1150", Db.Get().AccessorySlots.Hand);
@@ -84,14 +83,16 @@ namespace ONI_ARK
                     return; // 安全检查
                 }
 
+                Debug.Log($"当前模板的speech Mouth: {template.speech_mouth}");
+                Debug.Log($"当前模板的 Mouth: {template.mouth}");
                 // 2. 使用你查到的构造函数实例化自定义小人
                 Personality myCustomDupe1 = new Personality(
                     name_string_key: "MY_UNIQUE_DUPE_001", // 内部唯一ID，建议全大写且不包含空格
-                    name: "测试小人-阿尔法1",                  // 选人界面和游戏中实际显示的中文/英文名字
-                    Gender: template.genderStringKey,          // 借用性别标签
+                    name: "测试小人-阿尔法1", // 选人界面和游戏中实际显示的中文/英文名字
+                    Gender: template.genderStringKey, // 借用性别标签
                     PersonalityType: template.personalityType, // 借用性格类型
-                    StressTrait: template.stresstrait,         // 压力反应（如：破坏狂）
-                    JoyTrait: template.joyTrait,               // 喜悦反应（如：气球艺术家）
+                    StressTrait: template.stresstrait, // 压力反应（如：破坏狂）
+                    JoyTrait: template.joyTrait, // 喜悦反应（如：气球艺术家）
                     StickerType: template.stickerType,
                     CongenitalTrait: template.congenitaltrait, // 先天天赋
 
@@ -100,90 +101,90 @@ namespace ONI_ARK
                     mouth: template.mouth,
                     neck: 1150,
                     eyes: 1150,
-                    hair: template.hair,
+                    hair: 1150,
                     body: template.body,
                     belt: 1150,
                     cuff: 1150,
                     foot: 1150,
                     hand: template.hand,
                     pelvis: 1150,
-                    leg: 1150,  
+                    leg: 1150,
                     arm_skin: template.arm_skin,
                     leg_skin: template.leg_skin,
 
                     // 剩余配置
                     description: "这是一个带有自定义名字的测试复制人，目前借用了米普的身体！",
-                    isStartingMinion: true,           // 关键点：true 才能在开局选人界面刷出来
-                    graveStone: template.graveStone,  // 死亡后的墓碑样式
-                    model: template.model,            // 实体模型标签 (通常是 Minion)
+                    isStartingMinion: true, // 关键点：true 才能在开局选人界面刷出来
+                    graveStone: template.graveStone, // 死亡后的墓碑样式
+                    model: template.model, // 实体模型标签 (通常是 Minion)
                     SpeechMouth: template.speech_mouth // 说话时的嘴型动画索引
                 );
                 Personality myCustomDupe2 = new Personality(
-    name_string_key: "MY_UNIQUE_DUPE_002", // 内部唯一ID，建议全大写且不包含空格
-    name: "测试小人-阿尔法2",                  // 选人界面和游戏中实际显示的中文/英文名字
-    Gender: template.genderStringKey,          // 借用性别标签
-    PersonalityType: template.personalityType, // 借用性格类型
-    StressTrait: template.stresstrait,         // 压力反应（如：破坏狂）
-    JoyTrait: template.joyTrait,               // 喜悦反应（如：气球艺术家）
-    StickerType: template.stickerType,
-    CongenitalTrait: template.congenitaltrait, // 先天天赋
+                    name_string_key: "MY_UNIQUE_DUPE_002", // 内部唯一ID，建议全大写且不包含空格
+                    name: "测试小人-阿尔法2", // 选人界面和游戏中实际显示的中文/英文名字
+                    Gender: template.genderStringKey, // 借用性别标签
+                    PersonalityType: template.personalityType, // 借用性格类型
+                    StressTrait: template.stresstrait, // 压力反应（如：破坏狂）
+                    JoyTrait: template.joyTrait, // 喜悦反应（如：气球艺术家）
+                    StickerType: template.stickerType,
+                    CongenitalTrait: template.congenitaltrait, // 先天天赋
 
-    // 以下外观部位全部借用模板的整数ID (ID对应了材质图集中的部件)
-    headShape: template.headShape,
-    mouth: template.mouth, // 注意：字段名可能是 mouthShape，这里对应你构造函数的 mouth 参数
-    neck: template.neck,
-    eyes: template.eyes,
-    hair: template.hair,
-    body: template.body,
-    belt: template.belt,
-    cuff: template.cuff,
-    foot: template.foot,
-    hand: template.hand,
-    pelvis: template.pelvis,
-    leg: template.leg,
-    arm_skin: template.arm_skin,
-    leg_skin: template.leg_skin,
+                    // 以下外观部位全部借用模板的整数ID (ID对应了材质图集中的部件)
+                    headShape: template.headShape,
+                    mouth: template.mouth, // 注意：字段名可能是 mouthShape，这里对应你构造函数的 mouth 参数
+                    neck: template.neck,
+                    eyes: template.eyes,
+                    hair: template.hair,
+                    body: template.body,
+                    belt: template.belt,
+                    cuff: template.cuff,
+                    foot: template.foot,
+                    hand: template.hand,
+                    pelvis: template.pelvis,
+                    leg: template.leg,
+                    arm_skin: template.arm_skin,
+                    leg_skin: template.leg_skin,
 
-    // 剩余配置
-    description: "这是一个带有自定义名字的测试复制人，目前借用了米普的身体！",
-    isStartingMinion: true,           // 关键点：true 才能在开局选人界面刷出来
-    graveStone: template.graveStone,  // 死亡后的墓碑样式
-    model: template.model,            // 实体模型标签 (通常是 Minion)
-    SpeechMouth: template.speech_mouth // 说话时的嘴型动画索引
-);
+                    // 剩余配置
+                    description: "这是一个带有自定义名字的测试复制人，目前借用了米普的身体！",
+                    isStartingMinion: true, // 关键点：true 才能在开局选人界面刷出来
+                    graveStone: template.graveStone, // 死亡后的墓碑样式
+                    model: template.model, // 实体模型标签 (通常是 Minion)
+                    SpeechMouth: template.speech_mouth // 说话时的嘴型动画索引
+                );
                 Personality myCustomDupe3 = new Personality(
-    name_string_key: "MY_UNIQUE_DUPE_003", // 内部唯一ID，建议全大写且不包含空格
-    name: "测试小人-阿尔法3",                  // 选人界面和游戏中实际显示的中文/英文名字
-    Gender: template.genderStringKey,          // 借用性别标签
-    PersonalityType: template.personalityType, // 借用性格类型
-    StressTrait: template.stresstrait,         // 压力反应（如：破坏狂）
-    JoyTrait: template.joyTrait,               // 喜悦反应（如：气球艺术家）
-    StickerType: template.stickerType,
-    CongenitalTrait: template.congenitaltrait, // 先天天赋
+                    name_string_key: "MY_UNIQUE_DUPE_003", // 内部唯一ID，建议全大写且不包含空格
+                    name: "测试小人-阿尔法3", // 选人界面和游戏中实际显示的中文/英文名字
+                    Gender: template.genderStringKey, // 借用性别标签
+                    PersonalityType: template.personalityType, // 借用性格类型
+                    StressTrait: template.stresstrait, // 压力反应（如：破坏狂）
+                    JoyTrait: template.joyTrait, // 喜悦反应（如：气球艺术家）
+                    StickerType: template.stickerType,
+                    CongenitalTrait: template.congenitaltrait, // 先天天赋
 
-    // 以下外观部位全部借用模板的整数ID (ID对应了材质图集中的部件)
-    headShape: template.headShape,
-    mouth: template.mouth, // 注意：字段名可能是 mouthShape，这里对应你构造函数的 mouth 参数
-    neck: template.neck,
-    eyes: template.eyes,
-    hair: template.hair,
-    body: template.body,
-    belt: template.belt,
-    cuff: template.cuff,
-    foot: template.foot,
-    hand: template.hand,
-    pelvis: template.pelvis,
-    leg: template.leg,
-    arm_skin: template.arm_skin,
-    leg_skin: template.leg_skin,
+                    // 以下外观部位全部借用模板的整数ID (ID对应了材质图集中的部件)
+                    headShape: template.headShape,
+                    mouth: template.mouth, // 注意：字段名可能是 mouthShape，这里对应你构造函数的 mouth 参数
+                    neck: template.neck,
+                    eyes: template.eyes,
+                    hair: template.hair,
+                    body: template.body,
+                    belt: template.belt,
+                    cuff: template.cuff,
+                    foot: template.foot,
+                    hand: template.hand,
+                    pelvis: template.pelvis,
+                    leg: template.leg,
+                    arm_skin: template.arm_skin,
+                    leg_skin: template.leg_skin,
 
-    // 剩余配置
-    description: "这是一个带有自定义名字的测试复制人，目前借用了米普的身体！",
-    isStartingMinion: true,           // 关键点：true 才能在开局选人界面刷出来
-    graveStone: template.graveStone,  // 死亡后的墓碑样式
-    model: template.model,            // 实体模型标签 (通常是 Minion)
-    SpeechMouth: template.speech_mouth // 说话时的嘴型动画索引
-);
+                    // 剩余配置
+                    description: "这是一个带有自定义名字的测试复制人，目前借用了米普的身体！",
+                    isStartingMinion: true, // 关键点：true 才能在开局选人界面刷出来
+                    graveStone: template.graveStone, // 死亡后的墓碑样式
+                    model: template.model, // 实体模型标签 (通常是 Minion)
+                    SpeechMouth: template.speech_mouth // 说话时的嘴型动画索引
+                );
 
                 // 4. 最重要的一步：把他塞进游戏的选人池数据库里！
                 Db.Get().Personalities.Add(myCustomDupe1);
